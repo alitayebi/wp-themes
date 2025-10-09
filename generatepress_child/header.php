025fb5dcddc5acc10e4f01a6a60221409a03fabb
Ali Tayebi <42505961+alitayebi@users.noreply.github.com>
2025-10-09 15:56:14 +0200

    update title

--- generatepress_child/header.php
+++ generatepress_child/header.php
@@ -95,76 +95,89 @@ if (!defined('ABSPATH')) {
             ?>
           </nav>
         </div>
-        <div class="col-6">
-          <h1 class="fw-bold text-black2 page-title m-0"><?php _e('شیوه‌نامه‌ها', 'gp-child'); ?></h1>
-          <div class="text-muted fd">
-            <?php $pc = wp_count_posts('post')->publish;
-            printf(_n('%s مطلب', '%s مطلب', $pc, 'gp-child'), number_format_i18n($pc)); ?>
+        <div class="col-6" style="margin-top:140px;">
+          <div class="row">
+            <div class="col-6 col-lg-8">
+              <h1 class="fw-bold page-title m-0"><?php _e('شیوه‌نامه‌ها', 'gp-child'); ?></h1>
+              <div class="small text-muted fd">
+                <?php $pc = wp_count_posts('post')->publish;
+                printf(_n('%s مطلب', '%s مطلب', $pc, 'gp-child'), number_format_i18n($pc)); ?>
+              </div>
+            </div>
+            <div class="col-6 col-lg-4">
+              <div class="d-flex align-items-center gap-2 text-muted mt-auto">
+                <span>ترتیب</span>
+                <select class="form-select form-select-sm w-auto" onchange="location.href=this.value">
+                  <option value="<?php echo esc_url(home_url('/')); ?>"><?php _e('جدیدترین', 'gp-child'); ?></option>
+                  <option value="<?php echo esc_url(add_query_arg('orderby', 'random')); ?>">
+                    <?php _e('تصادفی', 'gp-child'); ?></option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <a class="navbar-brand brand" href="<?php echo esc_url(home_url('/')); ?>">
+              <?php if (is_front_page()): ?>
+                <!-- Front-page logo -->
+                <img src="https://simurghnameh.com/assets/logo-l0.png" alt="Simurghnameh" class="logo logo--front">
+              <?php else: ?>
+                <!-- Default logo -->
+                <img src="https://simurghnameh.com/assets/logo-inv.png" alt="Simurghnameh" class="logo logo--default">
+              <?php endif; ?>
+            </a>
           </div>
         </div>
-        <div class="col">
-          <a class="navbar-brand brand" href="<?php echo esc_url(home_url('/')); ?>">
-            <?php if (is_front_page()): ?>
-              <!-- Front-page logo -->
-              <img src="https://simurghnameh.com/assets/logo-l0.png" alt="Simurghnameh" class="logo logo--front">
-            <?php else: ?>
-              <!-- Default logo -->
-              <img src="https://simurghnameh.com/assets/logo-inv.png" alt="Simurghnameh" class="logo logo--default">
-            <?php endif; ?>
-          </a>
+
+
+        <div class="offcanvas offcanvas-top h-100" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+          <div class="">
+            <img class='mobile-menu-logo' src="https://simurghnameh.com/assets/logo-l02.png" class="layer-02">
+
+          </div>
+          <div class="offcanvas-body">
+
+            <?php
+            wp_nav_menu([
+              'theme_location' => 'primary',
+              'container' => false,
+              'items_wrap' => '<ul class="navbar-nav mx-0 pt-1 mb-lg-0 fw-bold">%3$s</ul>',
+              'fallback_cb' => false,
+              // wrap <a> text in a span with the class you want
+              'link_before' => '<span class="nav-link">',
+              'link_after' => '</span>',
+            ]);
+            ?>
+            <ul class="navbar-nav mx-0 mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="/about-en">about us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/about-us">درباره سیمرغنامه</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+      <?php if (is_singular('post')): ?>
+        <div class="container mt-auto pt-5">
+          <div class="row">
+            <div class="col-sm-8 px-4 mx-auto">
+              <h1><?php the_title(); ?></h1>
+              <div class="meta fd">
+                <?php
+                $author = get_the_author();
 
+                // relative time: "n days ago"
+                $relative = human_time_diff(get_the_time('U'), current_time('timestamp')) . ' ' . __('پیش', 'textdomain');
 
-      <div class="offcanvas offcanvas-top h-100" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-        <div class="">
-          <img class='mobile-menu-logo' src="https://simurghnameh.com/assets/logo-l02.png" class="layer-02">
-
-        </div>
-        <div class="offcanvas-body">
-
-          <?php
-          wp_nav_menu([
-            'theme_location' => 'primary',
-            'container' => false,
-            'items_wrap' => '<ul class="navbar-nav mx-0 pt-1 mb-lg-0 fw-bold">%3$s</ul>',
-            'fallback_cb' => false,
-            // wrap <a> text in a span with the class you want
-            'link_before' => '<span class="nav-link">',
-            'link_after' => '</span>',
-          ]);
-          ?>
-          <ul class="navbar-nav mx-0 mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="/about-en">about us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="/about-us">درباره سیمرغنامه</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <?php if (is_singular('post')): ?>
-      <div class="container mt-auto pt-5">
-        <div class="row">
-          <div class="col-sm-8 px-4 mx-auto">
-            <h1><?php the_title(); ?></h1>
-            <div class="meta fd">
-              <?php
-              $author = get_the_author();
-
-              // relative time: "n days ago"
-              $relative = human_time_diff(get_the_time('U'), current_time('timestamp')) . ' ' . __('پیش', 'textdomain');
-
-              echo esc_html(implode(' • ', array_filter([$author, $relative])));
-              ?>
-              <div class="float-end"><i class="bi bi-share-fill"></i></div>
+                echo esc_html(implode(' • ', array_filter([$author, $relative])));
+                ?>
+                <div class="float-end"><i class="bi bi-share-fill"></i></div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    <?php endif; ?>
+      <?php endif; ?>
   </header>
 
   <?php
