eed708fb2201b2bb4f502e899632ff82a4533f15
Ali Tayebi <42505961+alitayebi@users.noreply.github.com>
2025-10-10 05:35:26 +0200

    add footer menu

--- generatepress_child/functions.php
+++ generatepress_child/functions.php
@@ -5,3 +5,12 @@
  * Add your custom PHP in this file.
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
+function register_my_menus() {
+  register_nav_menus(
+    array(
+      'footer-menu1' => __( 'Footer Menu 1' ),
+      'footer-menu2' => __( 'Footer Menu 2' )
+    )
+  );
+}
+add_action( 'after_setup_theme', 'register_my_menus' );
