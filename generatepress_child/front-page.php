f01db208c0a8bb423673f240fa068ef69d69e651
Ali Tayebi <42505961+alitayebi@users.noreply.github.com>
2025-10-10 05:19:44 +0200

    add card as template part

--- generatepress_child/front-page.php
+++ generatepress_child/front-page.php
@@ -61,49 +61,8 @@ get_header(); ?>
         ?>
 
         <div class="<?php echo esc_attr($col_class); ?>">
-          <article class="mb-4">
-            <a href="<?php the_permalink(); ?>" class="d-block mb-2">
-              <?php if (has_post_thumbnail()) {
-                the_post_thumbnail($img_size, ['class' => $img_class]);
-              } else { ?>
-                <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/placeholder.png'); ?>"
-                  class="<?php echo esc_attr($img_class); ?>" alt="<?php esc_attr_e('Placeholder', 'gp-child'); ?>">
-              <?php } ?>
-            </a>
+          <?php get_template_part('template-parts/card', 'post'); ?>
 
-            <h3 class="fs-6 fw-bold mb-1">
-              <a class="link-dark text-decoration-none" href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-              </a>
-            </h3>
-
-            <div class="small fd text-muted">
-              <?php
-              echo esc_html(
-                sprintf(
-                  __('%s پیش', 'gp-child'),
-                  human_time_diff(get_the_time('U'), current_time('timestamp'))
-                )
-              );
-              ?>
-            </div>
-            <div>
-              <?php
-              $tags = get_the_tags();
-              if ( $tags ) {
-                $tags = array_slice( $tags, 0, 2 );
-                foreach ( $tags as $t ) {
-                  echo '<a class="btn btn-outline-dark btn-sm rounded-0" href="'.esc_url(get_tag_link($t)).'">'.esc_html($t->name).'</a>';
-                }
-              } else {
-                $cats_small = array_slice( get_the_category(), 0, 2 );
-                foreach ( $cats_small as $c ) {
-                  echo '<a class="btn btn-outline-dark btn-sm rounded-0" href="'.esc_url(get_category_link($c)).'">'.esc_html($c->name).'</a>';
-                }
-              }
-              ?>
-            </div>
-          </article>
         </div>
 
       <?php endwhile;
--- generatepress_child/single.php
+++ generatepress_child/single.php
@@ -29,52 +29,10 @@ $q    = new WP_Query([
 if ( $q->have_posts() ): ?>
 <section class="related my-5 py-3">
   <div class="container">
-    <h3 class="fs-5 fw-bold mb-3"><?php _e('مطالب مرتبط','simurgh'); ?></h3>
-
     <div class="row g-4 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
       <?php while ( $q->have_posts() ): $q->the_post(); ?>
       <div class="col">
-        <article class="card h-100 border-0 bg-transparent">
-          <!-- Image (1:1) -->
-          <a href="<?php the_permalink(); ?>">
-            <?php if ( has_post_thumbnail() ) {
-              the_post_thumbnail('related-thumb', ['class'=>'card-img-top img-fluid']);
-            } else { ?>
-              <img class="card-img-top img-fluid" src="<?php echo esc_url( get_stylesheet_directory_uri().'/placeholder.png'); ?>" alt="">
-            <?php } ?>
-          </a>
-
-          <div class="card-body">
-            <h4 class="card-title fs-6 fw-bold mb-1">
-              <a href="<?php the_permalink(); ?>" class="link-dark text-decoration-none text-reset"><?php the_title(); ?></a>
-            </h4>
-
-            <div class="text-muted fd small mb-2">
-              <?php
-                $ago = human_time_diff( get_the_time('U'), current_time('timestamp') );
-                echo esc_html( sprintf( __('%s پیش', 'simurgh'), $ago ) );
-              ?>
-            </div>
-
-            <!-- Labels (tags; fallback to categories) -->
-            <div>
-              <?php
-              $tags = get_the_tags();
-              if ( $tags ) {
-                $tags = array_slice( $tags, 0, 2 );
-                foreach ( $tags as $t ) {
-                  echo '<a class="btn btn-outline-dark btn-sm rounded-0" href="'.esc_url(get_tag_link($t)).'">'.esc_html($t->name).'</a>';
-                }
-              } else {
-                $cats_small = array_slice( get_the_category(), 0, 2 );
-                foreach ( $cats_small as $c ) {
-                  echo '<a class="btn btn-outline-dark btn-sm rounded-0" href="'.esc_url(get_category_link($c)).'">'.esc_html($c->name).'</a>';
-                }
-              }
-              ?>
-            </div>
-          </div>
-        </article>
+          <?php get_template_part('template-parts/card', 'post'); ?>
       </div>
       <?php endwhile; wp_reset_postdata(); ?>
     </div>
