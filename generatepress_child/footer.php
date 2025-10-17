40796eaabf98bec292d0d2ea3749f6f8deba50b9
Ali Tayebi <42505961+alitayebi@users.noreply.github.com>
2025-10-17 07:36:51 +0330

    change footer

--- generatepress_child/footer.php
+++ generatepress_child/footer.php
@@ -38,7 +38,7 @@
                     ]);
                     ?>
                 </div>
-                <div class="col-md mb-3 order-5 order-md-4">-</div>
+                <div class="col-md mb-3 order-5 order-md-4"></div>
                 <div class="col-md mb-3 order-4 order-md-5 text-center" dir="ltr" bis_skin_checked="1">
                     <p class="logo-wrapper">
                         <a href="/" class="logo"><img src="https://simurghnameh.com/assets/logo/logo-black.png"
